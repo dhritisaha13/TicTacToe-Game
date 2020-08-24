8d48a07a969c663d8ab21851b1c78c2aef122533
Dhriti Saha <dhritisaha16@gmail.com>
2020-08-24 21:53:57 +0530

    Updates

---
+++ precache-manifest.58573945cb4da052d2de90efe712c264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8573d5073ab8b7a4e09ed889fe4c8056",
+    "url": "/Tictactoe-Game/index.html"
+  },
+  {
+    "revision": "f4010995d35f057885b5",
+    "url": "/Tictactoe-Game/static/css/main.0f9c550e.chunk.css"
+  },
+  {
+    "revision": "4da593c3ea56796d746e",
+    "url": "/Tictactoe-Game/static/js/2.ea414d34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tictactoe-Game/static/js/2.ea414d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4010995d35f057885b5",
+    "url": "/Tictactoe-Game/static/js/main.4fd8e11f.chunk.js"
+  },
+  {
+    "revision": "179605f83dff44c74158",
+    "url": "/Tictactoe-Game/static/js/runtime-main.b79cf5c6.js"
+  }
+]);
